4ed14ea818d701a67670e0ffc31fe42155223693
Adrian Fraisse <adrian.fraisse@etudiant.mines-nantes.fr>
2015-08-02 20:50:48 +0530

    Initial Commit

---
+++ FunFacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunFacts
+//
+//  Created by Adrian Fraisse on 02/08/2015.
+//  Copyright (c) 2015 Adrian Fraisse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
